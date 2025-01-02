390aba815d50e810e3ae40c3efe727e37f5199fa
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-02 22:32:18 +0100

    Create get_punched.py

---
+++ CH03/get_punched.py
@@ -0,0 +1,32 @@
+def get_punched(health, armor=0):
+    damage = 50 - armor
+    new_health = health - damage
+    return new_health
+
+def get_slashed(health, armor=0):
+    damage = 100 - armor
+    new_health = health - damage
+    return new_health
+
+
+# Don't touch below this line
+
+
+def test(health, armor):
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after punch: {get_punched(health, armor)}")
+    print("=====================================")
+    print(f"Health: {health}, Armor: {armor}")
+    print(f"Health after slash: {get_slashed(health, armor)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after slash: {get_slashed(health)}\n")
+    print("=====================================")
+    print(f"Health: {health}, Armor: no armor!")
+    print(f"Health after punch: {get_punched(health)}")
+    print("=====================================")
+
+
+test(400, 5)
+test(300, 3)
+test(200, 1)
