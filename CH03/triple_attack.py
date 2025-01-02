17838a54dad7d4cb19eec61113e2897d949b95c3
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-02 22:23:17 +0100

    Create triple_attack.py

---
+++ CH03/triple_attack.py
@@ -0,0 +1,24 @@
+def triple_attack(damage_one, damage_two, damage_three):
+    sum = damage_one + damage_two + damage_three
+    return sum
+
+
+# Don't touch below this line
+
+attack_one = 2
+attack_two = 4
+attack_three = 3
+first_triple_attack_damage = triple_attack(attack_one, attack_two, attack_three)
+
+print("Getting damage for", attack_one, attack_two, "and", attack_three, "...")
+print(first_triple_attack_damage, "points of damage dealt!")
+print("=====================================")
+
+attack_four = -1
+attack_five = 10
+attack_six = 5
+second_triple_attack_damage = triple_attack(attack_four, attack_five, attack_six)
+
+print("Getting damage for", attack_four, attack_five, "and", attack_six, "...")
+print(second_triple_attack_damage, "points of damage dealt!")
+print("=====================================")
