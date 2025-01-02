93c54082f21748e0161f8a6ddf43c96243f37562
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-02 22:07:16 +0100

    Create hours_to_seconds.py

---
+++ hours_to_seconds.py
@@ -0,0 +1,17 @@
+def hours_to_seconds(hours):
+    return hours * 3600
+
+
+# Don't touch below this line
+
+
+def test(hours):
+    secs = hours_to_seconds(hours)
+    print(hours, "hours is", secs, "seconds")
+
+
+test(10)
+test(1)
+test(25)
+test(100)
+test(33)
