1982e719f9841a8a794db2603d11efb99383995f
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-13 23:32:07 +0100

    Update should_serve_customer.py

--- CH07/should_serve_customer.py
+++ CH07/should_serve_customer.py
@@ -1,1 +1,5 @@
-
+def should_serve_customer(customer_age, on_break, time):
+    if (customer_age >= 21 and on_break == False and time >= 5 and time <= 10) :
+        return True
+    else :
+        return False
