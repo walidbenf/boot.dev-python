39980cdcfbc2ddf36eb47014528e19c1da1595eb
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-29 18:27:04 +0100

    Create clear_inventory.py

---
+++ clear_inventory.py
@@ -0,0 +1,32 @@
+def clear_inventory():
+    inventory = [
+        "Healing Potion",
+        "Iron Bar",
+        "Kite Shield",
+        "Shortsword",
+        "Leather Scraps",
+        "Tattered Cloth",
+    ]
+
+    print(f"inventory: {inventory}")
+
+    # don't touch above this line
+
+    for i in range(0, len(inventory)):
+        item = inventory.pop()
+
+        # don't touch below this line
+        print(f"Selling: {item}")
+        print(f"inventory: {inventory}")
+
+
+def test():
+    clear_inventory()
+    print("=====================================")
+
+
+def main():
+    test()
+
+
+main()
