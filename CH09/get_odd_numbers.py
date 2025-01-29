ac295ea57b2b734ef48b86bb788a77a1e95aaeb1
walidbenf <45196746+walidbenf@users.noreply.github.com>
2025-01-29 18:48:42 +0100

    Create get_odd_numbers.py

---
+++ CH09/get_odd_numbers.py
@@ -0,0 +1,11 @@
+def get_odd_numbers(num):
+    odd_numbers = []
+
+    for i in range(0, num):
+        # don't touch above this line
+        if (i % 2 != 0):
+            odd_numbers.append(i)
+
+    # don't touch below this line
+
+    return odd_numbers
